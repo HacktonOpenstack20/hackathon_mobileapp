f9aeabe2dee0818b468f11278ce3862ef5ac957f
jonathan <jonathan.andrade@celeritech.com.br>
2017-12-02 15:30:32 -0200

    webView funcional e sincronização google firebase

---
+++ app/src/main/java/com/example/android/hackathon_open_mobile/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.android.hackathon_open_mobile;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.GetTokenResult;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        webView = (WebView) findViewById(R.id.webView1);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://10.20.3.146//");
+    }
+
+    
+}
